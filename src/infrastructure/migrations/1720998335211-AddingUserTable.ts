2d8d1a0a82a6039c0d6c1bd8b2efbe395ab530bb
silasoli <silasoliv39@gmail.com>
2024-07-14 20:15:51 -0300

    feat: finalize migration structure

---
+++ src/infrastructure/migrations/1720998335211-AddingUserTable.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddingUserTable1720998335211 implements MigrationInterface {
+  name = 'AddingUserTable1720998335211';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "salt" character varying NOT NULL, "password" character varying NOT NULL, "access_token" character varying, "refresh_token" character varying, "role" character varying, "expires_at" TIMESTAMP, "active" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_e12875dfb3b1d92d7d7c5377e2" ON "user" ("email") `,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_e12875dfb3b1d92d7d7c5377e2"`,
+    );
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
+}
