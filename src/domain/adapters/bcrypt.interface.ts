2ba80b34f0c3f5d30edbaa3344520f0631fd4ce8
silasoli <silasoliv39@gmail.com>
2024-07-19 22:29:02 -0300

    feat: adding functions to hash with salt and penauts

--- src/domain/adapters/bcrypt.interface.ts
+++ src/domain/adapters/bcrypt.interface.ts
@@ -1,4 +1,15 @@
+export interface IHashPassword {
+  salt: string;
+  hash: string;
+}
+
 export interface IBcryptService {
   hash(hashString: string): Promise<string>;
   compare(password: string, hashPassword: string): Promise<boolean>;
+  hashPassword(hashString: string): Promise<IHashPassword>;
+  comparePassword(
+    storedHash: string,
+    storedSalt: string,
+    providedPassword: string,
+  ): Promise<boolean>;
 }
--- src/infrastructure/services/bcrypt/bcrypt.service.ts
+++ src/infrastructure/services/bcrypt/bcrypt.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { IBcryptService } from '../../../domain/adapters/bcrypt.interface';
+import {
+  IBcryptService,
+  IHashPassword,
+} from '../../../domain/adapters/bcrypt.interface';
 
 @Injectable()
 export class BcryptService implements IBcryptService {
   rounds: number = 10;
 
   async hash(hashString: string): Promise<string> {
-    return await bcrypt.hash(hashString, this.rounds);
+    return bcrypt.hash(hashString, this.rounds);
   }
 
   async compare(password: string, hashPassword: string): Promise<boolean> {
-    return await bcrypt.compare(password, hashPassword);
+    return bcrypt.compare(password, hashPassword);
+  }
+
+  async hashPassword(password: string): Promise<IHashPassword> {
+    const salt = await bcrypt.genSalt(this.rounds);
+    const combined = salt + '123' + password;
+    const hash = await this.hash(combined);
+    return { salt, hash };
+  }
+
+  async comparePassword(
+    storedHash: string,
+    storedSalt: string,
+    providedPassword: string,
+  ): Promise<boolean> {
+    const combined = storedSalt + '123' + providedPassword;
+    return this.compare(combined, storedHash);
   }
 }
